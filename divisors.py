3bed3389d7568c5e70ef9d626d9805514e0b8952
Claire Laptop <clairenolan@hotmail.com>
2019-03-30 22:18:50 +0000

    Struggling with divisors. May be making it more complicated than required

--- divisors.py
+++ divisors.py
@@ -3,13 +3,21 @@
 # Question 3 of Problem Set
 # Ref: Lecture Notes and Python Tutorial
 
-for x in range (1000 , 10000):
-    while (x / 6) :
-        i = x / 6
-        print (int(i))
-        break
+for n in range(1000 , 10000):
 
-Print( ans ["This list are divisible by 6 but not by 12"])
+   for x in range(1000, n):
+
+      if n / 6 == int():
+
+        if n / 12 != int():
+                continue
+
+print (x, "is divisible by 6 but not 12")
+
+
+# For this problem i learnt to use the error message in CMDER and VSC to isentify where i was making errors especially syntax errors.
+
+
 
 
 
---
+++ second.py
@@ -0,0 +1,13 @@
+# Claire Nolan March 2019
+# sescond.py
+# Question 9 of Problem Set
+# Ref: Lecture Notes and Python Tutorial
+
+
+# insert referecne to moby text file
+# then ask to list every second word
+List (range (xxx))
+        if l / 12:
+  print(ans)
+
+
