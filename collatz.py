3192ec832507a4579558abf1d3ebdd8c4ad0fb3c
Claire Laptop <clairenolan@hotmail.com>
2019-03-30 23:20:47 +0000

    Inital attempt at Q4

--- collatz.py
+++ collatz.py
@@ -3,12 +3,16 @@
 # Question 4 of Problem Set
 # Ref: Lecture Notes and Python Tutorial, coders apprentice, "python in easy steps" by Mike Mcgrath"
 
+x = int(input("Please enter an integer: "))
 
+if x % 2 == 0:
+    i = x / 2
+    print(i)
+      
+if x % 2 != 0:
+        i = (x*3)+1
+        print(i)
 
+              
 
-
-
-
-
-
-
+# Initial attempt just returned 5.0
