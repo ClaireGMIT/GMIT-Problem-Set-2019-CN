16d6f0e985403f8db33ece882f7eb6e0fc0e1be6
Claire Laptop <clairenolan@hotmail.com>
2019-03-30 20:41:35 +0000

    Redoing and completing questions after solving my Github priblems.

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,13 +1,18 @@
 # Claire Nolan March 2019
-# Python begins-with-t.py
+# begins-with-t.py
 # Question 2 of Problem Set
 # Ref: Lecture Notes and Python Tutorial
 
-x = int(input("Please enter todays day: "))
+import datetime
 
-if x == T:
+if datetime.datetime.today().weekday() == 1:
   print("Today begins with T")
+
+
+if datetime.datetime.today().weekday() == 3:
+  print("Today begins with T")
+
 else:
   print("Today does NOT begin with T")
 
-  # moving on to next question while i figure this out
+# It took a while to figure this out but i re-watched the tutorials and figured it out. It probably could be simplified by copying/repeating some of the programme. I'll fine tune this later.
