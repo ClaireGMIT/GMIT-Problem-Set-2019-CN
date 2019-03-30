3d1b1bb47e6212913f3da71fec68061dbefe22ae
Claire Laptop <clairenolan@hotmail.com>
2019-03-30 21:11:46 +0000

    Still working out Q3 of problem set

--- divisors.py
+++ divisors.py
@@ -3,14 +3,13 @@
 # Question 3 of Problem Set
 # Ref: Lecture Notes and Python Tutorial
 
-n = 6
-
 for x in range (1000 , 10000):
-    while (x /= 6) :
-        i = x /= 6
+    while (x / 6) :
+        i = x / 6
         print (int(i))
         break
 
-Print ("This list are divisible by 6 but not by 12")
+Print( ans ["This list are divisible by 6 but not by 12"])
+
 
 
