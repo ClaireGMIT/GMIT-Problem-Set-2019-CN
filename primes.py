8e3cc276aadc5370e485177c47982c5204a7d56e
Claire Laptop <clairenolan@hotmail.com>
2019-03-31 02:36:29 +0100

    Question 5 complete

--- primes.py
+++ primes.py
@@ -7,10 +7,15 @@
 
 i = int(input("Please enter a positive integer: "))
 
-for n in range(2, i):
-    for x in range(2, n):
-        if n % x != 0:
-            print(n, 'is a prime number')
-            break
-    
+for x in range(2 , i):
+    if i % x == 0:
+        print(i, "is not a prime number")
+        break
+            
+else:
+    if i % x != 0:
+        print(i, "is a prime number")
+
 # 1. Initial programme returned list from 3 to 18      
+# 2. I need to get used to using the right symbols ie (, :) etc in the right places.
+# 3. Realised i had the order wrong for the if and else statements. Once fixed program ran smoothly
