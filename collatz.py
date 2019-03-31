fbd6844750670feba3453c4c5906554039854f9e
Claire Laptop <clairenolan@hotmail.com>
2019-03-31 00:01:47 +0000

    Q4 again

--- collatz.py
+++ collatz.py
@@ -5,14 +5,22 @@
 
 x = int(input("Please enter an integer: "))
 
+
 if x % 2 == 0:
-    i = x / 2
-    print(i)
-      
-if x % 2 != 0:
-        i = (x*3)+1
-        print(i)
+    i = (x/2)
+#Initially getting as far as this step. Will look at indents. Have decided to remove "print (i)" command.  
+        
+elif x % 2 != 0:
+            i = (x*3)+1
+            print(i)
 
-              
+else:
+    if x == 1:
 
-# Initial attempt just returned 5.0
+break
+
+# i'm going to restart and try "while" loops
+# Initial attempt just returned 5.0. Issue may be around getting correct indentation.
+
+
+
