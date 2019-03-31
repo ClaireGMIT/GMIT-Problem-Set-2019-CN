d744da6d4181803c842fc7ffa93b6baf3413eade
Claire Laptop <clairenolan@hotmail.com>
2019-03-31 12:54:40 +0100

    Beginning prep for last 5 Q

---
+++ datetime.py
@@ -0,0 +1,13 @@
+# Claire Nolan March 2019
+# secondstring.py
+# Question 8 of Problem Set
+# Ref: Lecture Notes and Python Tutorial, coders apprentice, "python in easy steps" by Mike Mcgrath"
+# specifically used the standard library section of the Python tutorial
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+
---
+++ secondstring.py
@@ -0,0 +1,36 @@
+# Claire Nolan March 2019
+# secondstring.py
+# Question 6 of Problem Set
+# Ref: Lecture Notes and Python Tutorial, coders apprentice, "python in easy steps" by Mike Mcgrath"
+# specifically used the standard library section of the Python tutorial
+
+
+s = (input("Please enter a sentence: "))
+
+s.split[(2)]
+
+
+
+print(s)
+
+
+            
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
